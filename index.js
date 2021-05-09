fe0336c0545c2f9356b940536b63f5f0e0cfc794
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-09 22:51:21 +0530

    added animation to h2

---
+++ index.js
@@ -0,0 +1,6 @@
+// window.onload = setTimeout(()=>{
+//     document.querySelector(".second-heading").classList.add("expand")
+// }, 00)
+
+
+window.onload = document.querySelector(".second-heading").classList.add("expand")
