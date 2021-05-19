fcc99762c08c27cf539ed34663a23db8f7f82f5c
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-19 07:28:46 +0530

    added slider to portfolio

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// window.onload = setTimeout(()=>{
-//     document.querySelector(".second-heading").classList.add("expand")
-// }, 00)
-
-
-window.onload = document.querySelector(".second-heading").classList.add("expand")
+console.log("inside js")
+var arrow1 = document.querySelector(".arrow-1");
+var set1 = document.querySelector(".set-1");
+var set2 = document.querySelector(".set-2");
+arrow1.addEventListener("click", () => {
+    console.log("inside event listner")
+    set1.style.transform="translateX(-500px)"
+    setTimeout(()=> set1.style.display = "none", 300)
+    set2.style.transform="translateX(0px)"
+    setTimeout(()=> set2.style.display = "block", 300)
+    arrow1.style.display="none"
+    arrow2.style.display="inline-block"
+})
