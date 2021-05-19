f71b6bc0c86d2825653ec8f46cb5608717ddca2b
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-19 08:04:50 +0530

    portfolio part done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("inside js")
 var arrow1 = document.querySelector(".arrow-1");
+var arrow2 = document.querySelector(".arrow-2");
 var set1 = document.querySelector(".set-1");
 var set2 = document.querySelector(".set-2");
 arrow1.addEventListener("click", () => {
@@ -9,5 +10,15 @@ arrow1.addEventListener("click", () => {
     set2.style.transform="translateX(0px)"
     setTimeout(()=> set2.style.display = "block", 300)
     arrow1.style.display="none"
-    arrow2.style.display="inline-block"
+    arrow2.style.display="block"
+})
+
+arrow2.addEventListener("click", () => {
+    console.log("inside event listner")
+    set1.style.transform="translateX(0px)"
+    setTimeout(()=> set1.style.display = "block", 300)
+    set2.style.transform="translateX(800px)"
+    setTimeout(()=> set2.style.display = "none", 300)
+    arrow1.style.display="block"
+    arrow2.style.display="none"
 })
