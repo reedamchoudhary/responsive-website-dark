e8139b6d964d48c3c308eacae9e3d4828adf2d97
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-20 08:59:23 +0530

    visibility of about page completed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var arrow2 = document.querySelector(".arrow-2");
 var set1 = document.querySelector(".set-1");
 var set2 = document.querySelector(".set-2");
 var button = document.querySelector(".message button");
+var aboutMe = document.getElementById("selfinfo");
+var closeAbout = document.getElementById("aboutId");
 
 
 arrow1.addEventListener("click", () => {
@@ -26,8 +28,16 @@ arrow2.addEventListener("click", () => {
     arrow2.style.display="none"
 })
 
-button.addEventListener("click", () => {
-    
+aboutMe.addEventListener("click", () => {
+    document.querySelector(".inner-about").style.opacity = "1";
+    document.querySelector(".section-1").style.transform = "scale(1)";
+    document.querySelector(".about-me").style.visibility = "visible"
+})
+
+closeAbout.addEventListener("click", () => {
+    document.querySelector(".inner-about").style.opacity = "0";
+    document.querySelector(".section-1").style.transform = "scale(0)";
+    document.querySelector(".about-me").style.visibility = "hidden"
 })
 
 // arrow1.addEventListener("mouseover",()=>{
