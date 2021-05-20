aec77b18a04a203460f9167324a600a8347cc0c0
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-20 09:11:10 +0530

    portfolio visibility done

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var set2 = document.querySelector(".set-2");
 var button = document.querySelector(".message button");
 var aboutMe = document.getElementById("selfinfo");
 var closeAbout = document.getElementById("aboutId");
+var work = document.getElementById("work");
+var closeWork = document.getElementById("workId");
 
 
 arrow1.addEventListener("click", () => {
@@ -38,6 +40,18 @@ closeAbout.addEventListener("click", () => {
     document.querySelector(".inner-about").style.opacity = "0";
     document.querySelector(".section-1").style.transform = "scale(0)";
     document.querySelector(".about-me").style.visibility = "hidden"
+})
+
+work.addEventListener("click", () => {
+    document.querySelector(".sets").style.opacity = "1";
+    document.querySelector(".section-2").style.transform = "scale(1)";
+    document.querySelector(".portfolio-sec").style.visibility = "visible"
+})
+
+closeWork.addEventListener("click", () => {
+    document.querySelector(".sets").style.opacity = "0";
+    document.querySelector(".section-2").style.transform = "scale(0)";
+    document.querySelector(".portfolio-sec").style.visibility = "hidden"
 })
 
 // arrow1.addEventListener("mouseover",()=>{
